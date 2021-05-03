34d444b5eeaebf66473e3b5d7a2e24f8b402411f
Sebastian Martin Muñoz <sebamm1999@gmail.com>
2021-05-03 14:50:46 -0400

    Lista la función app(counter)

--- counter.js
+++ counter.js
@@ -1,16 +1,24 @@
 function view(counter) {
-    return ("   Count: " +counter+"\n   (+) (-)\n   (q) for quit")
+    return ("   Count: " +counter+"\n   (+) (-)\n   (q) for quit");
 }
 
-function update(msg, counter){
-    counter = 0
+function update(msg, counter) {
     if (msg == "+")
-        return counter += 1
+        return counter += 1;
     if (msg == "-")
-        return counter -= 1
-    else;
-        return counter = "counter"
+        return counter -= 1;
+    else
+        return counter;
 }
 
-const prompt = require("prompt-sync")(); 
-const msg = prompt("What would you do? ");
+function app(counter){
+    while(true) {
+        const currentView = view(counter);
+        console.clear()
+        console.log(currentView);
+        const prompt = require("prompt-sync")({sigint: true});
+        const msg = prompt("What would you do? ");
+        counter = update(msg, counter);
+    }
+}
+app(0)
